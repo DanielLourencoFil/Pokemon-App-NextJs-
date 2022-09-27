78590985ec3fc97effa9318766ae597c4bc592e4
daniellourencoedu <daniellourencoedu@gmail.com>
2022-09-27 15:13:54 -0300

    pokemon app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Themeprovider } from "../theme/Themeprovider";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<Themeprovider>
+			<Component {...pageProps} />
+		</Themeprovider>
+	);
 }
 
-export default MyApp
+export default MyApp;
